0da4eced6b8abeba033636ac2ee40b23219eea64
DECLAN NNADOZIE <ZEDONBOY063@GMAIL.COM>
2018-08-18 23:14:01 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+(function (){
+    
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        var NaijaState = String(element)
+        var tmp = NaijaState.length
+        for (let w = index; w > 0 && tmp < ; w++) {
+            
+            
+        }
+        
+    }
+})()
